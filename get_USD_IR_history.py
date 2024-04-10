5c85794234df78ad4566bc848ca0e28dee5c9ad9
Behnam <Do you really need my email>
2024-04-10 18:58:48 +0100

    add comment to the code

---
+++ get_USD_IR_history.py
@@ -0,0 +1,51 @@
+import pandas as pd 
+import requests
+from sklearn.preprocessing import MinMaxScaler
+import warnings
+warnings.filterwarnings('ignore')
+
+import time
+
+current_unixtime = int(time.time())
+
+url = 'https://dashboard-api.tgju.org/v1/tv/history?symbol=PRICE_DOLLAR_RL&resolution=1D&from=1262304000&to=' + str(current_unixtime)
+try:
+    res = requests.get(url=url)
+    res_j = res.json()
+except requests.exceptions.RequestException as e:
+    print(f"Error occurred during request: {e}")
+
+# Get the column names from the dictionary
+column_names = list(res_j.keys())
+
+res_j.pop('s')
+res_j.pop('v')
+
+# Create the DataFrame
+try:
+  hisotry_raw = pd.DataFrame.from_dict(res_j)
+except pd.errors.ParserError as e:
+  print(f"Error occurred while parsing the data: {e}")
+
+# Convert the unix timestamp column to datetime
+try:
+  hisotry_raw['t'] = pd.to_datetime(hisotry_raw['t'],unit='s')
+except pd.errors.OutOfBoundsDatetime as e:
+  print(f"Error occurred while converting the timestamp: {e}")
+
+hisotry_raw.to_csv('hisotry_raw.csv', index=False)
+# Select the relevant columns
+history_processed = hisotry_raw[['t','c']].copy()
+# Rename the columns
+history_processed.rename(columns={'t':'ds','c':'y'}, inplace= True)
+# Filter the data to start from 2017-01-01
+history_processed = history_processed[history_processed['ds'] >= '2012-01-01'].reset_index().drop(columns='index')
+# Drop duplicates
+history_processed = history_processed.drop_duplicates(subset=['ds'])
+
+scaler = MinMaxScaler()
+history_processed["y"] = scaler.fit_transform(history_processed["y"].values.reshape(-1, 1))
+
+# Save the DataFrame to a CSV file
+history_processed.to_csv('history_processed.csv', index=False)
+history_processed
